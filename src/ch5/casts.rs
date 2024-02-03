e7d8443ecbf1847f4fe036fddf6ecb78044f860c
Fei Chen <hiflike@126.com>
2024-02-03 15:10:54 +0800

    cast

---
+++ src/ch5/casts.rs
@@ -0,0 +1,8 @@
+#[test]
+fn cast_test(){
+    let a:i32 =12;
+    let b:u32 = a as u32;
+
+    println!("a:{},b:{}",a,b);
+    println!("a:{},b:{}",a,b);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 pub mod ch2;
 pub mod ch3;
-pub mod ch4;
+pub mod ch5;
